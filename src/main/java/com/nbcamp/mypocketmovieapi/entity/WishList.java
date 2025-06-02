ff5e18025a3263e9a087cc12c29c3303a62b1016
ddagom <mstbright.249@gmail.com>
2025-06-02 13:47:45 +0900

    위시리스트 엔티티 등록

--- src/main/java/com/nbcamp/mypocketmovieapi/entity/WishList.java
+++ src/main/java/com/nbcamp/mypocketmovieapi/entity/WishList.java
@@ -2,6 +2,7 @@ package com.nbcamp.mypocketmovieapi.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,9 +10,28 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "wishlists")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class WishList extends BaseEntity {
+public class Wishlist extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id", nullable = false)
+    private Content content;
+
+    @Column(name = "external_id", nullable = false)
+    private String externalId;
+
+    @Builder
+    public Wishlist(Member member, Content content, String externalId) {
+        this.member = member;
+        this.content = content;
+        this.externalId = externalId;
+    }
 }
