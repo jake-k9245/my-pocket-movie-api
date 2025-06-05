53e2e45b0ce6263a7f56c80d8eb261cad2bf751e
mychemicalcode <jty7027@gmail.com>
2025-06-05 14:55:07 +0900

    Feture #9 Content에 Review 작성 API 구현 V3

--- src/main/java/com/nbcamp/mypocketmovieapi/service/ReviewService.java
+++ src/main/java/com/nbcamp/mypocketmovieapi/service/ReviewService.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ReviewService {
     private final ReviewJpaRepository reviewRepository;
     private final MemberJpaRepository memberRepository;
