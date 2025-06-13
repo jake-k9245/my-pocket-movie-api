84462af5c8ebf36ff8b6ed8cae2a8dfe4ba90c0f
playkoround <seasouphan@gmail.com>
2025-06-13 12:17:26 +0900

    Feat: Add handleDuplicateEmail with GlobalExceptionHandler
    Github issue #24

---
+++ src/main/java/com/nbcamp/mypocketmovieapi/exception/member/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.nbcamp.mypocketmovieapi.exception.member;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+
+    @ExceptionHandler(DuplicateEmailException.class)
+    public ResponseEntity<?> handleDuplicateEmail(DuplicateEmailException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of(
+                "code", 409,
+                "message", e.getMessage()
+        ));
+    }
+}
