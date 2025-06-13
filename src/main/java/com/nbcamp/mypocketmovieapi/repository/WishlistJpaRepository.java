8c6a62ae5ca4748f37c656d69f1335000e10e521
ddagom <mstbright.249@gmail.com>
2025-06-13 12:13:30 +0900

    위시리스트 추가 #23

--- src/main/java/com/nbcamp/mypocketmovieapi/repository/WishlistJpaRepository.java
+++ src/main/java/com/nbcamp/mypocketmovieapi/repository/WishlistJpaRepository.java
@@ -1,8 +1,12 @@
 package com.nbcamp.mypocketmovieapi.repository;
 
+import com.nbcamp.mypocketmovieapi.entity.Member;
 import com.nbcamp.mypocketmovieapi.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WishlistJpaRepository extends JpaRepository<Wishlist, Long> {
+import java.util.List;
 
+public interface WishlistJpaRepository extends JpaRepository<Wishlist, Long> {
+    List<Wishlist> findByMember(Member member);
+    boolean existsByMemberAndExternalId(Member member, String externalId);
 }
