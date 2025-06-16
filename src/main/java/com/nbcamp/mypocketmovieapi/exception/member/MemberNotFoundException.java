25510705ff7c5ed214db847c18f65765f5984cd2
playkoround <seasouphan@gmail.com>
2025-06-16 10:42:16 +0900

    Feat: Add exception classes and CommonCode
    Github issue #32

--- src/main/java/com/nbcamp/mypocketmovieapi/exception/member/MemberNotFoundException.java
+++ src/main/java/com/nbcamp/mypocketmovieapi/exception/member/MemberNotFoundException.java
@@ -3,13 +3,13 @@ package com.nbcamp.mypocketmovieapi.exception.member;
 import com.nbcamp.mypocketmovieapi.common.CommonCode;
 
 public class MemberNotFoundException extends RuntimeException {
-    private final CommonCode code;
+    private final CommonCode commonCode;
 
-    public MemberNotFoundException(CommonCode code) {
-        this.code = code;
+    public MemberNotFoundException(CommonCode commonCode) {
+        this.commonCode = commonCode;
     }
 
     public CommonCode getCommonCode() {
-        return this.code;
+        return this.commonCode;
     }
 }
---
+++ src/main/java/com/nbcamp/mypocketmovieapi/exception/member/SignInInvalidPassword.java
@@ -0,0 +1,15 @@
+package com.nbcamp.mypocketmovieapi.exception.member;
+
+import com.nbcamp.mypocketmovieapi.common.CommonCode;
+
+public class SignInInvalidPassword extends RuntimeException {
+    private CommonCode commonCode;
+
+    public SignInInvalidPassword(CommonCode code){
+        commonCode = code;
+    }
+
+    public CommonCode getCommonCode() {
+        return commonCode;
+    }
+}
